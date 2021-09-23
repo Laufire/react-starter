a26338a9dc7273c8cd4faf8c391f910555f19f07
nkg <nkg@laufire.in>
2021-09-23 08:37:40 +0000

    Refactored fresh script.

--- fresh.sh
+++ fresh.sh
@@ -3,13 +3,29 @@ cd "$(dirname "$0")"
 
 # Tasks
 mergeBranch() {
-	git merge fresh
+	git fetch origin fresh
+	git checkout fresh
+	git checkout master
+	git merge fresh || true
+}
+
+executeScripts(){
+	sh ./adopt.sh
+	sh ./setup.sh
+}
+
+initializeCommit() {
+	git add .
+	git commit -m "Initialized."
 }
 
 deleteScripts() {
+	rm ./examples.sh
 	rm ./fresh.sh
 }
 
 # Main
 mergeBranch
+executeScripts
 deleteScripts
+initializeCommit
