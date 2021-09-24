815162f35c4ddc727cf4bb4fafb1d8d0a958e683
nkg <nkg@laufire.in>
2021-09-24 10:42:40 +0000

    BugFix setup.sh.

--- fresh.sh
+++ fresh.sh
@@ -1,11 +1,18 @@
 set -e
 cd "$(dirname "$0")"
 
+removeConflictFile() {
+	rm ./setup.sh
+	git add .
+	git commit -m "temp"
+}
+
 # Tasks
 mergeBranch() {
 	git fetch origin fresh
 	git checkout fresh
 	git checkout master
+	removeConflictFile
 	git merge fresh || true
 }
 
