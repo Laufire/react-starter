5c6d3878224a5e118e0a11ba2cfed1127dce6d0d
nkg <nkg@laufire.in>
2021-09-30 07:34:43 +0000

    Dusted.

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,5 @@
+jest.mock('./core/context', () => ({ config: { message: 'message' }}));
+
 import { React } from 'react';
 import { render, screen } from '@testing-library/react';
 
@@ -5,7 +7,7 @@ import App from './App';
 
 test('renders learn react link', () => {
 	render(<App/>);
-	const someText = screen.getByText(/hello/);
+	const someText = screen.getByText('message');
 
 	expect(someText).toBeInTheDocument();
 });
