d0711e5e9e763c6d4826de4c062b4005ae74dc40
Laufire <laufiretechnologies@gmail.com>
2021-10-02 00:16:06 +0530

    Streamlined with master.

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import context from './core/context';
 
-test('component renders with welcome message', () => {
-	render(App(context));
-	const someText = screen.getByText(context.config.message);
+describe('App', () => {
+	test('renders with a welcome message', () => {
+		render(App(context));
+		const someText = screen.getByText(context.config.message);
 
-	expect(someText).toBeInTheDocument();
+		expect(someText).toBeInTheDocument();
+	});
 });
--- src/core/context.js
+++ src/core/context.js
@@ -1,10 +1,12 @@
 import actions from './actions';
 import config from './config';
+import init from './init';
 import seed from './seed';
 
 const context = {
 	actions,
 	config,
+	init,
 	seed,
 };
 
