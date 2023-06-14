befaac8fbaed0694fce817d7069b8fa2b83e3af1
nkg-abs <nkg@abs.in>
2023-06-14 17:52:06 +0530

    Removed reportWebVitals.js.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import updateContext from '@laufire/resist';
 import './index.css';
 import App from './App';
@@ -19,8 +18,3 @@ ReactDOM.render(<React.StrictMode>
 	<Entry/>
 </React.StrictMode>,
 document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
